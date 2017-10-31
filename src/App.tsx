510d7d7386aa66dad276071667320b7249c180db
Tomáš Holas <tomas.holas@gmail.com>
2017-10-31 14:08:53 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+const styles = require('./App.module.css');
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className={styles.AppIntro}>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
